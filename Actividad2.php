fdc89632d26e617993a9797404a5d88a07883dc9
unknown <tamara_arenas@comunidad.unam.mx>
2020-05-14 13:03:58 -0500

    FEAT: guradé Activividad2.php

---
+++ Actividad2.php
@@ -0,0 +1,5 @@
+  <?php
+     echo "Arenas Vázquez Tamara Isabel";
+     echo "<br>";
+     echo "En este curso he aprendido a tener una alta tolerancia a la frustración y a buscar que mi código solucione todas las situaciones.";
+   ?>
